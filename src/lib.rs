a65193cc871744062ae803ceb2fa1ae095cfd02b
th0rex <omit2912@web.de>
2017-03-02 17:38:54 +0100

    Implement more iter functions

--- src/lib.rs
+++ src/lib.rs
@@ -48,6 +48,62 @@ impl cs_arm {
     }
 }
 
+impl cs_arm64 {
+    pub fn operand_iter(&self) -> PointerIter<cs_arm64_op> {
+        PointerIter::new(self.operands.as_ptr(), self.op_count as _)
+    }
+}
+
+impl cs_mips {
+    pub fn operand_iter(&self) -> PointerIter<cs_mips_op> {
+        PointerIter::new(self.operands.as_ptr(), self.op_count as _)
+    }
+}
+
+impl cs_ppc {
+    pub fn operand_iter(&self) -> PointerIter<cs_ppc_op> {
+        PointerIter::new(self.operands.as_ptr(), self.op_count as _)
+    }
+}
+
+impl cs_sparc {
+    pub fn operand_iter(&self) -> PointerIter<cs_sparc_op> {
+        PointerIter::new(self.operands.as_ptr(), self.op_count as _)
+    }
+}
+
+impl cs_sysz {
+    pub fn operand_iter(&self) -> PointerIter<cs_sysz_op> {
+        PointerIter::new(self.operands.as_ptr(), self.op_count as _)
+    }
+}
+
+impl cs_x86 {
+    pub fn operand_iter(&self) -> PointerIter<cs_x86_op> {
+        PointerIter::new(self.operands.as_ptr(), self.op_count as _)
+    }
+}
+
+impl cs_xcore {
+    pub fn operand_iter(&self) -> PointerIter<cs_xcore_op> {
+        PointerIter::new(self.operands.as_ptr(), self.op_count as _)
+    }
+}
+
+impl cs_detail {
+    pub fn regs_read_iter(&self) -> PointerIter<u8> {
+        PointerIter::new(self.regs_read.as_ptr(), self.regs_read_count as _)
+    }
+
+    pub fn regs_write_iter(&self) -> PointerIter<u8> {
+        PointerIter::new(self.regs_write.as_ptr(), self.regs_write_count as _)
+    }
+
+    pub fn groups_iter(&self) -> PointerIter<u8> {
+        PointerIter::new(self.groups.as_ptr(), self.groups_count as _)
+    }
+}
+
 use std::fmt;
 impl fmt::Debug for cs_insn {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
