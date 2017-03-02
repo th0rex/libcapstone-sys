3ccf856cfff20047c70db7317f6e675f64ad2adf
th0rex <omit2912@web.de>
2017-03-02 17:23:13 +0100

    Add PointerIter

--- src/lib.rs
+++ src/lib.rs
@@ -6,20 +6,38 @@
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
+/// `PointerIter` iterates over an array of things that are
+/// pointed to by an pointer.
+pub struct PointerIter<'a, T: 'a> {
+    ptr: &'a *const T,
+    count: usize,
+    current: usize,
+}
+
+impl<'a, T: 'a> Iterator for PointerIter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<&'a T> {
+        if self.current >= self.count {
+            None
+        } else {
+            let object = unsafe { &*self.ptr.offset(self.current as _) };
+            self.current += 1;
+            Some(object)
+        }
+    }
+}
+
 use std::fmt;
 impl fmt::Debug for cs_insn {
-    fn fmt(&self, f: &mut fmt::Formatter) -> 
-     fmt::Result {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         f.debug_struct("cs_insn")
             .field("id", &self.id)
-            
-     .field("address", &self.address)
+            .field("address", &self.address)
             .field("size", &self.size)
-            
-     .field("bytes", &self.bytes)
+            .field("bytes", &self.bytes)
             .field("mnemonic", &self.mnemonic)
-            
-     .field("detail", &self.detail)
+            .field("detail", &self.detail)
             .finish()
     }
 }
